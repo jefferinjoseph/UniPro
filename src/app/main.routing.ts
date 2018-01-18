6d0c1b742cc167eacb0d13b289511a1764162be5
Jefferin Joseph <jefferin.joseph@dh.com>
2018-01-18 10:03:43 +0530

    Login component with login

--- src/app/main.routing.ts
+++ src/app/main.routing.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test-component/test.component';
+import { LoginComponent } from "./login/login.component";
 
 export const routes: Routes = [
     { path: '', component: AppComponent },
-    { path: 'test', component: TestComponent }
+    { path: 'test', component: TestComponent },
+    { path: 'login', component: LoginComponent }
   ];
 
   export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
